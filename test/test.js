51949883199e98078c916c66ad0b8c8b35446032
Deepa-Bala <deepabala875@gmail.com>
2020-10-13 17:04:54 +0530

    Update test

--- test/test.js
+++ test/test.js
@@ -5,10 +5,6 @@ const request = require("supertest");
       it("respond with Hello World", (done) => {
       
         request(app).get("/")
-                    .expect("OCRtoJsonDataProcess", done)
-                    .end(function(err, res) {
-                        if (err) return done(err);
-                        done();
-                       });
+                    .expect("OCRtoJsonDataProcess", done);                    
       })
     });
